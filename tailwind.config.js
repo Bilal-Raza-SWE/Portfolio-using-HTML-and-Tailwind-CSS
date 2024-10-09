2608cb60af538a1e258bbd4e422a6082192c882e
Bilal-Raza-SWE <bilalrazaswe@gmail.com>
2024-10-10 00:43:03 +0500

    Add necessary files. These files are not compulsory to run project you can delete theem. Just run html files and enjoy

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './*.html',  // Adjust this path if your HTML files are in subdirectories
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
